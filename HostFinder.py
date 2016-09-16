a375b714b42a449c43a8c5f0f505a86a97a41219
fatih,kayran <kayranfatih@hotmail.com>
2016-09-16 13:32:48 +0300

    ARP Ping Scan Tool

--- HostFinder.py
+++ HostFinder.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env
 
-import sys, getopt
+import sys, getopt, logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
 
 interface = "eth0"
 scannableIP = "192.168.0.0"
@@ -28,8 +30,22 @@ def GetArgument():
 			scannableCIDR = arg.split('/')[1]
 		elif opt in ('-h'):
 			print helpSection
-
+			exit(0)
 
 GetArgument()
 
-print interface + " " + scannableIP + " " + scannableCIDR	
+try :
+	
+	ans,unans=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=scannableIP + "/" + scannableCIDR), timeout=3, iface=interface, verbose=0)
+
+	print "-"*41 + "\nInterface : " + interface + " " + "Range : " +scannableIP + "/" + scannableCIDR + "\n" + "-"*41
+	print " "*3 + "Mac Adress" + " "*6 + "IP Adress\n"
+
+	for s, r in ans : 
+		print r.sprintf("%Ether.src% %ARP.psrc%")
+
+except socket.error :
+	print "Invalid interface / IP"
+
+except ValueError :
+	print "Invalid IP/CIDR format (e.g 192.168.1.0/24)"
